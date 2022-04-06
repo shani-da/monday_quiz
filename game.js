d89b94bee67c1c8b508630ac95b820d93fba283d
shani-da <87568347+shani-da@users.noreply.github.com>
2022-04-06 16:36:40 +0300

    Update game.js

--- game.js
+++ game.js
@@ -18,7 +18,6 @@ var interval = setInterval(function(){
   document.getElementById('count').innerHTML=count;
   count--;
   if (count === 0){
-    clearInterval(interval);
     document.getElementById('count').innerHTML='Done';
     getNewQuestion();
     count = 15;
