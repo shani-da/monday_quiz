42303d5c181baa6e2a64af8ff77b183d1f451e00
shani-da <87568347+shani-da@users.noreply.github.com>
2022-04-08 18:16:59 +0300

    Update game.js

--- game.js
+++ game.js
@@ -179,7 +179,7 @@ getNewQuestion = () => {
     
 
     let el = document.getElementById('choice3')
-    if((document.getElementById('choice1') == 'TRUE') || (count_choices<4)) {
+    if((document.getElementById('choice1') == 'TRUE' || 'FALSE') || (count_choices<4)) {
         document.getElementsByClassName('choice-container2').visibility='hidden';
         document.getElementById('choice3').style.display = 'none';
         document.getElementById('choice4').style.visibility='hidden';
