4734e001629f618591e88aa6e2cfe0fdc880a143
shani-da <87568347+shani-da@users.noreply.github.com>
2022-04-08 18:37:58 +0300

    Update game.js

--- game.js
+++ game.js
@@ -177,7 +177,7 @@ getNewQuestion = () => {
     
 
     let el = document.getElementById('choice3').textContent;
-    if(el === undefined) {
+    if(el === undefined || el === null) {
         document.getElementById('choice3').style.display = 'none';
         document.getElementById('choice4').style.visibility='hidden';
     }
