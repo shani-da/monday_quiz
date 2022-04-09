01664ba1dc1ef5c5dbd8482cd8e39b4d71bc7561
shani-da <87568347+shani-da@users.noreply.github.com>
2022-04-09 15:02:35 +0300

    Update game.js

--- game.js
+++ game.js
@@ -38,7 +38,7 @@ fetch(
                 difficulty: loadedQuestion.difficulty,
                 category: loadedQuestion.category,
                 question: loadedQuestion.question.replace(/(&quot\;)/g,"\"").replace(/(&#039\;)/g,"\'").replace(/(&amp\;)/g,"&")
-                .replace(/(&deg\;)/g,"°").replace(/(&rsquo\;)/g,"\'").replace(/(&shy\;)/g,"-").replace(/(&Eacute\;)/g,"É").replace(/(&ntilde\;)/g,"ñ"),
+                .replace(/(&deg\;)/g,"°").replace(/(&rsquo\;)/g,"\'").replace(/(&shy\;)/g,"-").replace(/(&Eacute\;)/g,"É").replace(/(&ntilde\;)/g,"ñ").replace(/(&prime\;)/g,"′").replace(/(&eacute\;)/g,"É").replace(/(&ouml\;)/g,"Ö").replace(/(&uuml\;)/g,"Ü"),
             };
             
             const answerChoices = [...loadedQuestion.incorrect_answers];
@@ -48,12 +48,12 @@ fetch(
                 0,
                 
                 loadedQuestion.correct_answer.replace(/(&quot\;)/g,"\"").replace(/(&#039\;)/g,"\'").replace(/(&amp\;)/g,"&")
-                .replace(/(&deg\;)/g,"°").replace(/(&rsquo\;)/g,"\'").replace(/(&shy\;)/g,"-").replace(/(&Eacute\;)/g,"É").replace(/(&ntilde\;)/g,"ñ")
+                .replace(/(&deg\;)/g,"°").replace(/(&rsquo\;)/g,"\'").replace(/(&shy\;)/g,"-").replace(/(&Eacute\;)/g,"É").replace(/(&ntilde\;)/g,"ñ").replace(/(&prime\;)/g,"′").replace(/(&eacute\;)/g,"É").replace(/(&ouml\;)/g,"Ö").replace(/(&uuml\;)/g,"Ü")
             );
             
             answerChoices.forEach((choice, index) => {
                 formattedQuestion['choice' + (index + 1)] = choice.replace(/(&quot\;)/g,"\"").replace(/(&#039\;)/g,"\'").replace(/(&amp\;)/g,"&")
-                .replace(/(&deg\;)/g,"°").replace(/(&rsquo\;)/g,"\'").replace(/(&shy\;)/g,"-").replace(/(&Eacute\;)/g,"É").replace(/(&ntilde\;)/g,"ñ");
+                .replace(/(&deg\;)/g,"°").replace(/(&rsquo\;)/g,"\'").replace(/(&shy\;)/g,"-").replace(/(&Eacute\;)/g,"É").replace(/(&ntilde\;)/g,"ñ").replace(/(&prime\;)/g,"′").replace(/(&eacute\;)/g,"É").replace(/(&ouml\;)/g,"Ö").replace(/(&uuml\;)/g,"Ü");
             });
         //cat    
         //    const category = loadedQuestion.category;
